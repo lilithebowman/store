9bb2b5445c39f653e2b9fe816897ca1fe469b3bd
Lilithe 🐰🥕💜 <lilithe.bowman@gmail.com>
2025-05-31 18:41:10 -0400

    Port changes

--- client/src/hooks/useProducts.js
+++ client/src/hooks/useProducts.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || `${window.location.hostname}:2048/api`;
 
 const useProducts = () => {
 	const [products, setProducts] = useState([]);
