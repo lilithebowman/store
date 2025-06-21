70824e168a1c4b6ba6e6d0c9f783b9334ce5cb77
Lilithe Bowman <lilithe.bowman@gmail.com>
2025-06-21 18:31:06 -0400

    Feature (#23)
    * User Profile Updates - Image WIP
    
    * User profile image changes
    
    * Time to go to bed
    
    * Fixed profile image upload
    
    * Profile crown
    
    * WIP User Role Controller
    
    * RBAC Attack!
    
    * Utility script to delete all users
    
    * Update User DB Table
    
    * Created User Management and Role Management Pages
    
    * Fixed User and Roile listing
    
    * Use BASE URL
    
    * Profile.jsx: Update user profile page
    
    * Fixed Roles on UIserpage
    
    * Filling out logic for Products and Pages listings
    
    * Page support fleshed out. Pages route from /page/slug-name here
    
    * WIP page builder with Storybook feature

---
+++ client/src/components/pageBuilder/PageRenderer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Box, Container, Alert } from '@mui/material';
+import { renderComponent } from './ComponentRegistry';
+
+const PageRenderer = ({ components = [], containerProps = {} }) => {
+	if (!Array.isArray(components)) {
+		console.error('PageRenderer: components prop must be an array');
+		return <Alert severity="error">Invalid page configuration</Alert>;
+	}
+
+	return (
+		<Container {...containerProps}>
+			<Box sx={{ py: 2 }}>
+				{components.length === 0 ? (
+					<Alert severity="info">
+						This page has no components yet.
+					</Alert>
+				) : (
+					components.map((component, index) => (
+						<Box key={component.id || index} sx={{ mb: 2 }}>
+							{renderComponent(component)}
+						</Box>
+					))
+				)}
+			</Box>
+		</Container>
+	);
+};
+
+export default PageRenderer;
